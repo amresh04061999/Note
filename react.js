bae45fc968aa4fe86e79b167a44f4d00629feeb9
Amresh_Prasad <108825632+amresh04061999@users.noreply.github.com>
2024-11-20 15:55:49 +0530

    Create react.js

---
+++ react.js
@@ -0,0 +1,12 @@
+jsx is merge logic and view together in single file
+jsx is like html but not html
+babel javascript transpile is convert jsx code to react code and react element is js object so browser understand 
+lean about babal, react,parcel,builder,vite,webpack,
+class in html but jsx usind className
+jsx write multiple line so used (curly bracket)
+extention for better comments ,eslint,brekect
+//React component 
+class base  or function base compnenet
+class base is old version used but latest version used function base 
+react function is like normal javascript function that is return jsx code
+*component composition is one component inside another component
